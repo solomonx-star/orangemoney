4b34f6b6b58bdc415ca70a32f28917ef1251dbfc
solomonx-star <solomoncaster523@gmail.com>
2023-07-25 00:19:41 +0000

    Update

--- src/components/my account.js
+++ src/components/my account.js
@@ -11,70 +11,70 @@ import { s } from 'react-native-wind';
 const Account = () =>{
     return(
         <ScrollView>
-        <View className = "">
-            <View className ={s `flex-row mt-4 ml-3`}>
+        <View style = "">
+            <View style ={s `flex-row mt-4 ml-3`}>
                 <Icon name = 'user' size = {25} color = '' />
-                <Text className = "ml-3  text-base font-semibold">079 55 08 16</Text>
+                <Text style ={s `ml-3  text-base font-semibold`}>079 55 08 16</Text>
             </View>
-            <View className = "ml-3">
+            <View style ={s `ml-3`}>
                 <TouchableOpacity>
-                    <Text className = "text-orange-500 mt-1 font-bold">Log out</Text>
+                    <Text style ={s `text-orange-500 mt-1 font-bold`}>Log out</Text>
                 </TouchableOpacity>
             </View>
             <View>
-                <View className = "bg-white h-screen mt-4">
-                    <View className = "ml-3 flex-row">
-                        <Text className = "text-slate-400 mt-3 font-bold">Payment methods</Text>
+                <View style ={s `bg-white h-screen mt-4`}>
+                    <View style ={s `ml-3 flex-row`}>
+                        <Text style ={s `text-slate-400 mt-3 font-bold`}>Payment methods</Text>
                     </View>
-                    <View className = "mt-10 ml-3 flex-row">
-                        <View className = "bg-slate-200 h-10 w-10"></View>
-                        <Text className = "mt-2 ml-3 text-base font-bold">QR Code reader</Text>
-                        <View className = "mt-3 ml-48">
+                    <View style ={s `mt-10 ml-3 flex-row`}>
+                        <View style ={s `bg-slate-200 h-10 w-10`}></View>
+                        <Text style ={s `mt-2 ml-3 text-base font-bold`}>QR Code reader</Text>
+                        <View style ={s `mt-3 ml-48`}>
                             <Icon name="chevron-right" size={15}  />
                         </View>
                     </View>
-                    <View className = "border mt-7 border-gray-300"></View>
-                    <View className = "ml-3 flex-row">
-                        <Text className = "text-slate-400 mt-3 font-bold">Settings</Text>
+                    <View style ={s `border mt-7 border-gray-300`}></View>
+                    <View style ={s `ml-3 flex-row`}>
+                        <Text style ={s `text-slate-400 mt-3 font-bold`}>Settings</Text>
                     </View>
-                    <View className = "mt-10 ml-3 flex-row">
-                        <View className = "bg-slate-200 h-10 w-10"></View>
-                        <Text className = "mt-2 ml-3 text-base font-bold">Change secret code</Text>
-                        <View className = "mt-3 ml-40">
+                    <View style ={s `mt-10 ml-3 flex-row`}>
+                        <View style ={s `bg-slate-200 h-10 w-10`}></View>
+                        <Text style ={s `mt-2 ml-3 text-base font-bold`}>Change secret code</Text>
+                        <View style ={s `mt-3 ml-40`}>
                             <Icon name="chevron-right" size={15}  />
                         </View>
                     </View>
-                    <View className = "mt-5 ml-3 flex-row">
-                        <View className = "bg-slate-200 h-10 w-10"></View>
-                        <Text className = "mt-2 ml-3 text-base font-bold">Touch ID</Text>
-                        <View className = "mt-3 ml-64">
+                    <View style ={s `mt-5 ml-3 flex-row`}>
+                        <View style ={s `bg-slate-200 h-10 w-10`}></View>
+                        <Text style ={s `mt-2 ml-3 text-base font-bold`}>Touch ID</Text>
+                        <View style ={s `mt-3 ml-64`}>
                             <Icon name="chevron-right" size={15}  />
                         </View>
                     </View>
-                    <View className = "border mt-10 border-gray-300"></View>
-                    <View className = "ml-3 flex-row">
-                        <Text className = "text-slate-400 mt-3 font-bold">Help and contact</Text>
+                    <View style ={s `border mt-10 border-gray-300`}></View>
+                    <View style ={s `ml-3 flex-row`}>
+                        <Text style ={s `text-slate-400 mt-3 font-bold`}>Help and contact</Text>
                     </View>
-                    <View className = "mt-10 ml-3 flex-row">
-                        <View className = "bg-slate-200 h-10 w-10">
+                    <View style ={s `mt-10 ml-3 flex-row`}>
+                        <View style ={s `bg-slate-200 h-10 w-10`}>
                         </View>
-                        <Text className = "mt-2 ml-3 text-base font-bold">Share the App</Text>
-                        <View className = "mt-3 ml-48">
+                        <Text style ={s `mt-2 ml-3 text-base font-bold`}>Share the App</Text>
+                        <View style ={s `mt-3 ml-48`}>
                             <Icon name="chevron-right" size={15}  />
                         </View>
                     </View>
-                    <View className = "mt-5 ml-3 flex-row">
-                        <View className = "bg-slate-200 h-10 w-10"></View>
-                        <Text className = "mt-2 ml-3 text-base font-bold">Contact us</Text>
-                        <View className = "mt-3 ml-56">
+                    <View style ={s `mt-5 ml-3 flex-row`}>
+                        <View style ={s `bg-slate-200 h-10 w-10`}></View>
+                        <Text style ={s `mt-2 ml-3 text-base font-bold`}>Contact us</Text>
+                        <View style ={s `mt-3 ml-56`}>
                             <Icon name="chevron-right" size={15}  />
                         </View>
                     </View>
-                    <View className = "mt-5 ml-3 flex-row">
-                        <View className = "bg-slate-200 h-10 w-10"></View>
-                        <Text className = "mt-2 ml-3 text-base font-bold">About</Text>
-                        <View className = "mt-3 ml-64">
-                            <Icon name="chevron-right" size={15}  />
+                    <View style ={s `mt-5 ml-3 flex-row`}>
+                        <View style ={s `bg-slate-200 h-10 w-10`}></View>
+                        <Text style ={s `mt-2 ml-3 text-base font-bold`}>About</Text>
+                        <View style ={s `mt-3 ml-64`}>
+                            <Icon name="chevron-right" size={15} />
                         </View>
                     </View>
                 </View>
