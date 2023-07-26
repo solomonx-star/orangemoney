9b5928d04453e85516684f2a4038bc0642be4ee2
solomonx-star <solomoncaster523@gmail.com>
2023-07-26 11:34:45 +0000

    update

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import React from 'react'
 import TabNav from './src/components/Navigation/TabNav'
 import { NavigationContainer } from '@react-navigation/native'
@@ -6,6 +6,7 @@ import { NavigationContainer } from '@react-navigation/native'
 const App = () => {
   return (
     <NavigationContainer>
+    <StatusBar />
     <TabNav/>
 </NavigationContainer>
   )
